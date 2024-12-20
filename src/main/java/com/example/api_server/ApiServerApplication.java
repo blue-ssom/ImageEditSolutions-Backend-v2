99a63e89c530b92c076d11cf11a4ce42bb4ead60
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-12-20 09:57:26 +0900

    chore: 스프링 프로젝트 초기 설정

---
+++ src/main/java/com/example/api_server/ApiServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.api_server;
+
+import lombok.ToString.Exclude;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerApplication.class, args);
+	}
+
+}
