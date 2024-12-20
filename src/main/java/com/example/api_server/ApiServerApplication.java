399e71df251ef4fd2598708efcfc0ab071665757
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-12-20 10:00:06 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/example/api_server/ApiServerApplication.java
+++ src/main/java/com/example/api_server/ApiServerApplication.java
@@ -1,6 +1,5 @@
 package com.example.api_server;
 
-import lombok.ToString.Exclude;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
