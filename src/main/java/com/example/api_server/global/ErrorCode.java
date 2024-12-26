a8584374f8f304d2da6ace5ff0a0bde68e4be79e
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-12-26 15:21:54 +0900

    feat: ROOM_NOT_FOUND 에러 코드 추가

--- src/main/java/com/example/api_server/global/ErrorCode.java
+++ src/main/java/com/example/api_server/global/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH-001", "아이디 또는 비밀번호를 잘못 입력했습니다."),
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "AUTH-002", "이미 존재하는 아이디입니다."),
-    EMPTY_USERNAME(HttpStatus.BAD_REQUEST, "AUTH-003", "아이디는 필수 입력값입니다.");
+    EMPTY_USERNAME(HttpStatus.BAD_REQUEST, "AUTH-003", "아이디는 필수 입력값입니다."),
+
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND,"ROOM-001", "방을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus; // HTTP 상태 코드
     private final String code;           // 에러 코드
