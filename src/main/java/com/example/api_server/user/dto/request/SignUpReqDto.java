2f8708d42b2186758efb1271d5854d18f9fe15fe
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-12-30 16:27:35 +0900

    feat: SignUpReqDto에 중복 확인 여부 필드 추가

--- src/main/java/com/example/api_server/user/dto/request/SignUpReqDto.java
+++ src/main/java/com/example/api_server/user/dto/request/SignUpReqDto.java
@@ -14,8 +14,12 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SignUpReqDto {
 
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
     private String username;
 
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
+
+    private Boolean isChecked; // 중복 확인 여부
 
 }
