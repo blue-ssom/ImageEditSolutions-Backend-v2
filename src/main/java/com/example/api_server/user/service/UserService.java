e78e61bc733a7b18c71929d61d91e284f503afa7
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-12-30 16:26:49 +0900

    refactor: UserService 인터페이스의 주석 제거

--- src/main/java/com/example/api_server/user/service/UserService.java
+++ src/main/java/com/example/api_server/user/service/UserService.java
@@ -5,7 +5,7 @@ import com.example.api_server.user.dto.request.SignUpReqDto;
 import com.example.api_server.global.response.ResponseDto;
 
 public interface UserService {
-    ResponseDto login(LoginReqDto loginReqDto); // LoginReqDto를 인자로 받도록 정의
+    ResponseDto login(LoginReqDto loginReqDto);
     ResponseDto signUp(SignUpReqDto signUpReqDto);
     ResponseDto checkUsername(String username);
 }
