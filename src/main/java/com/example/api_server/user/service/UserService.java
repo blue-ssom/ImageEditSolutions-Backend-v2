5cc745209e6079145e910cd1ed5a92626991ba71
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-12-30 16:45:13 +0900

    refactor: checkUsername 메서드의 파라미터를 DTO로 변경

--- src/main/java/com/example/api_server/user/service/UserService.java
+++ src/main/java/com/example/api_server/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.api_server.user.service;
 
+import com.example.api_server.user.dto.request.CheckUsernameReqDto;
 import com.example.api_server.user.dto.request.LoginReqDto;
 import com.example.api_server.user.dto.request.SignUpReqDto;
 import com.example.api_server.global.response.ResponseDto;
@@ -7,5 +8,5 @@ import com.example.api_server.global.response.ResponseDto;
 public interface UserService {
     ResponseDto login(LoginReqDto loginReqDto);
     ResponseDto signUp(SignUpReqDto signUpReqDto);
-    ResponseDto checkUsername(String username);
+    ResponseDto checkUsername(CheckUsernameReqDto checkUsernameReqDto);
 }
