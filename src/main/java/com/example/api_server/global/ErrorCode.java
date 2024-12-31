4827f532ec01035b21de753c68e991d1a4591af0
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-12-31 14:09:05 +0900

    feat: JWT 관련 에러 코드 추가

--- src/main/java/com/example/api_server/global/ErrorCode.java
+++ src/main/java/com/example/api_server/global/ErrorCode.java
@@ -13,7 +13,13 @@ public enum ErrorCode {
     CHECK_NOT_PERFORMED(HttpStatus.BAD_REQUEST,"AUTH-004","아이디 및 이메일 중복 확인이 필요합니다."),
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "AUTH-005", "이미 존재하는 아이디입니다."),
 
-    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND,"ROOM-001", "방을 찾을 수 없습니다.");
+    ROOM_NOT_FOUND(HttpStatus.NOT_FOUND,"ROOM-001", "방을 찾을 수 없습니다."),
+
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-001", "유효하지 않은 JWT 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-002", "JWT 토큰이 만료되었습니다."),
+    UNSUPPORTED_TOKEN(HttpStatus.BAD_REQUEST, "JWT-003", "지원하지 않는 JWT 토큰입니다."),
+    EMPTY_CLAIMS(HttpStatus.BAD_REQUEST, "JWT-004", "JWT 클레임 문자열이 비어있습니다."),
+    UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "JWT-999", "예기치 않은 서버 오류");
 
     private final HttpStatus httpStatus; // HTTP 상태 코드
     private final String code;           // 에러 코드
