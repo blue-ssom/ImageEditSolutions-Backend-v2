27df686968af303293270afca7b5295e5f58d1f1
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2025-01-01 21:30:51 +0900

    feat: 새로운 에러 코드 추가

--- src/main/java/com/example/api_server/global/ErrorCode.java
+++ src/main/java/com/example/api_server/global/ErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    NEED_LOGIN(HttpStatus.UNAUTHORIZED,"AUTH-000","로그인이 필요합니다."),
     EMPTY_USERNAME(HttpStatus.BAD_REQUEST,"AUTH-001","아이디는 필수 입력값입니다."),
     EMPTY_PASSWORD(HttpStatus.BAD_REQUEST, "AUTH-002","비밀번호는 필수 입력값입니다."),
     USER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "AUTH-003", "아이디 또는 비밀번호를 잘못 입력했습니다."),
@@ -14,12 +15,7 @@ public enum ErrorCode {
     DUPLICATE_USERNAME(HttpStatus.CONFLICT, "AUTH-005", "이미 존재하는 아이디입니다."),
 
     ROOM_NOT_FOUND(HttpStatus.NOT_FOUND,"ROOM-001", "방을 찾을 수 없습니다."),
-
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-001", "유효하지 않은 JWT 토큰입니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "JWT-002", "JWT 토큰이 만료되었습니다."),
-    UNSUPPORTED_TOKEN(HttpStatus.BAD_REQUEST, "JWT-003", "지원하지 않는 JWT 토큰입니다."),
-    EMPTY_CLAIMS(HttpStatus.BAD_REQUEST, "JWT-004", "JWT 클레임 문자열이 비어있습니다."),
-    UNEXPECTED_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "JWT-999", "예기치 않은 서버 오류");
+    ROOM_ALREADY_EXISTS(HttpStatus.BAD_REQUEST,"ROOM-002","이미 존재하는 방 번호입니다.");
 
     private final HttpStatus httpStatus; // HTTP 상태 코드
     private final String code;           // 에러 코드
