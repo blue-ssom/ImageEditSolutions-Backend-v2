ae469b686719948e3e9fe5b083c4f9fab7bfab69
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2025-01-02 23:19:17 +0900

    refactor: EditingController 리팩토링 및 메시지 처리 방식 변경

--- src/main/java/com/example/api_server/editing/controller/EditingController.java
+++ src/main/java/com/example/api_server/editing/controller/EditingController.java
@@ -1,23 +1,37 @@
 package com.example.api_server.editing.controller;
 
-import com.example.api_server.editing.entity.Editing;
-import com.example.api_server.room.service.impl.RoomServiceImpl;
+import com.example.api_server.editing.entity.EditedImage;
+import com.example.api_server.editing.service.EditingService;
+import java.util.HashMap;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class EditingController {
 
-    private final RoomServiceImpl roomServiceImpl;
-    @MessageMapping("/edit/{roomId}") // 클라이언트가 "/app/edit/{roomId}"로 보낸 메시지를 처리
-    @SendTo("/topic/edit/{roomId}")  // 구독 중인 클라이언트들에게 메시지 전송
-    public Editing handleEditMessage(@DestinationVariable String roomId, @Payload Editing drawData) {
-        System.out.println("Room ID: " + roomId + ", Draw Data: " + drawData);
-        return drawData; // 클라이언트들에게 데이터 반환
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final EditingService editingService;
+
+    @MessageMapping("/room/{roomNumber}/edit") // 클라이언트가 이 경로로 메시지를 보냄
+    public void sendImageEditToAllClients(@DestinationVariable String roomNumber,
+            @Payload EditedImage editedImage) {
+
+//        String userId = (String) session.getAttribute("userId");
+
+        System.out.println("joined room " + roomNumber);
+        System.out.println("Received data: " + editedImage);
+
+        // 그리기 데이터 추가 및 갱신
+        editingService.addDrawingCache(roomNumber, editedImage);
+
+        // 모든 클라이언트에게 편집된 이미지를 전송
+        simpMessagingTemplate.convertAndSend("/topic/room/" + roomNumber + "/edit", editedImage);
     }
+
 }
